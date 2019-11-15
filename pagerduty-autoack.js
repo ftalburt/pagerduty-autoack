24b06e22afd9de458c9a2c8f9c81a54c2e3de3a5
Forrest Talburt <2643911+ftalburt@users.noreply.github.com>
2019-11-15 16:49:02 -0600

    Fix bugs and add ability to filter by service

--- pagerduty-autoack.js
+++ pagerduty-autoack.js
@@ -4,32 +4,50 @@ const sleep = util.promisify(setTimeout);
 
 if (process.env.APITOKEN && process.env.REGEX) {
   const pdapiToken = process.env.APITOKEN;
-  const regex = process.env.REGEX;
+  const regex = new RegExp(process.env.REGEX);
   const desiredStatus = process.env.UPDATESTATUS || "acknowledged";
   const queryInterval = process.env.QUERYINTERVAL || 10000;
+  const servicesFilter = process.env.SERVICES
+    ? process.env.SERVICES.split(",")
+    : undefined;
   const pd = new pdClient(pdapiToken);
-  processIncidents(pd, desiredStatus, regex, queryInterval);
+  processIncidents(pd, desiredStatus, regex, queryInterval, servicesFilter);
 } else {
   console.error("FATAL ERROR: Missing env var for APITOKEN or REGEX");
   process.exit(1);
 }
 
-async function processIncidents(pdClient, desiredStatus, regex, queryInterval) {
-  console.log("Checking to see who this API key belongs to")
+async function processIncidents(
+  pdClient,
+  desiredStatus,
+  regex,
+  queryInterval,
+  servicesFilter
+) {
+  console.log("Checking to see who this API key belongs to");
   let currentUser = JSON.parse((await pdClient.users.getCurrentUser()).body)
     .user;
   let currentUserId = currentUser.id;
   let currentUserEmail = currentUser.email;
   console.log(`API key belongs to ${currentUserEmail}`);
+
+  let serviceIds;
+  if (servicesFilter) {
+    serviceIds = JSON.parse((await pd.services.listServices()).body)
+      .services.filter(service => servicesFilter.includes(service))
+      .map(service => service.id);
+  }
+
   for (;;) {
     try {
-      console.log("Checking for triggered incidents")
+      console.log("Checking for triggered incidents");
       let allTriggeredIncidents = JSON.parse(
         (
           await pdClient.incidents.listIncidents({
             date_range: "all",
             statuses: ["triggered"],
-            user_ids: [currentUserId]
+            user_ids: [currentUserId],
+            services_ids: serviceIds
           })
         ).body
       ).incidents;
@@ -57,7 +75,7 @@ async function processIncidents(pdClient, desiredStatus, regex, queryInterval) {
       console.error(error);
     }
 
-    console.log(`Sleeping ${queryInterval}ms`)
+    console.log(`Sleeping ${queryInterval}ms`);
     await sleep(queryInterval);
   }
 }
